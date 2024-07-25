c8f52f7f8eb170aa243fc30f3ddabd990fed3e1f
elias <borrajo.elias@gmail.com>
2024-07-25 23:35:42 +0200

    backend fonctionelle, manque plus que récuperer les files de OUTPUT

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Description: Build the backend docker image.
 # Give the current user id and group id as build arguments to set the user in the container.
-docker build --network=host --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -t backend-app .
+
+# Change to the directory where the Dockerfile is located
+cd "/home/elias/tbSectra/backend/sectraplugin_backend"
+
+# Build the image
+docker build --network=host --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg DOCKER_GID=1003 -f "API REST/Dockerfile" -t api-rest .

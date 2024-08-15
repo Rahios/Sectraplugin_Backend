6934bd38fe58a1d490092115cbc75d87e800183a
elias <borrajo.elias@gmail.com>
2024-08-15 15:52:27 +0200

    commentaires & doc

--- BLL/Interfaces/IHistolungManager.cs
+++ BLL/Interfaces/IHistolungManager.cs
@@ -2,11 +2,35 @@
 
 namespace BLL.Interfaces
 {
+	/// <summary>
+	/// Interface defining the methods for managing histological image analysis and retrieving associated data.
+	/// </summary>
 	public interface IHistolungManager
 	{
-		global::DTO.HistolungResponse AnalyseImage(global::DTO.HistolungRequest request);
+
+		/// <summary>
+		/// Analyzes a histological image based on the parameters specified in the request.
+		/// </summary>
+		/// <param name="request">The request containing the necessary information for image analysis.</param>
+		/// <returns>A <see cref="HistolungResponse"/> object containing the results of the analysis.</returns>
+		HistolungResponse AnalyseImage(global::DTO.HistolungRequest request);
+
+		/// <summary>
+		/// Retrieves the heatmap of the last image analysis.
+		/// </summary>
+		/// <returns>A byte array representing the heatmap image.</returns>
 		byte[] GetHeatmap();
+
+		/// <summary>
+		/// Retrieves the list of available image names for analysis.
+		/// </summary>
+		/// <returns>An array of strings containing the names of available images.</returns>
 		string[] GetImagesList();
+
+		/// <summary>
+		/// Retrieves the results of the last analysis performed.
+		/// </summary>
+		/// <returns>A <see cref="HistolungResponse"/> object containing the results of the last analysis.</returns>
 		HistolungResponse GetLastAnalysis();
 	}
 }
--- DAL/Interfaces/IHistolungFS.cs
+++ DAL/Interfaces/IHistolungFS.cs
@@ -2,11 +2,34 @@
 
 namespace DAL.Interfaces
 {
+	/// <summary>
+	/// Interface defining methods for interacting with the file system to perform histological image analysis and retrieve related data.
+	/// </summary>
 	public interface IHistolungFS
 	{
-		global::DTO.HistolungResponse AnalyzeImage(global::DTO.HistolungRequest request);
+		/// <summary>
+		/// Analyzes a histological image based on the provided request.
+		/// </summary>
+		/// <param name="request">The request containing details of the image to be analyzed.</param>
+		/// <returns>A <see cref="HistolungResponse"/> containing the analysis results, including predictions and heatmap data.</returns>
+		HistolungResponse AnalyzeImage(HistolungRequest request);
+
+		/// <summary>
+		/// Retrieves the heatmap image from the last analysis as a byte array.
+		/// </summary>
+		/// <returns>A byte array representing the heatmap image.</returns>
 		byte[] GetHeatmap();
+
+		/// <summary>
+		/// Retrieves the list of available images to be analyzed from the file system.
+		/// </summary>
+		/// <returns>An array of strings containing the names of available images.</returns>
 		string[] GetImagesList();
+
+		/// <summary>
+		/// Retrieves the results of the last analysis performed.
+		/// </summary>
+		/// <returns>A <see cref="HistolungResponse"/> containing the results of the last analysis.</returns>
 		HistolungResponse GetLastAnalysis();
 	}
 }
